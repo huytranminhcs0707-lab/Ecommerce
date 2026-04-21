dd22188a649aa1046376dcbac39f18bbd1558ae8
huy.tranminhcs0707@hcmut.edu.vn <huy.tranminhcs0707@hcmut.edu.vn>
2026-04-21 14:18:33 +0700

    Use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import { Header } from '../components/Header';
 import './HomePage.css';
 import { products } from '../../starting-code/data/products.js'
+import axios from 'axios'
 export function HomePage() {
+    axios.get('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response.data);
+        });
     return (
         <>
             <title>Ecommerce Project</title>
